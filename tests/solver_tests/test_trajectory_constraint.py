b16f22fdaf057af738eea87ab346213010aab712
HiroIshida <spitfire.docomo@gmail.com>
2022-12-28 20:36:18 +0900

    test: add test_trajectory_constraint

---
+++ tests/solver_tests/test_trajectory_constraint.py
@@ -0,0 +1,44 @@
+from typing import Tuple
+
+import numpy as np
+
+from skmp.constraint import AbstractIneqConst
+from skmp.solver.trajectory_constraint import TrajectoryInequalityConstraint
+
+
+class CircleConstraint(AbstractIneqConst):
+    def evaluate(self, qs: np.ndarray, with_jacobian: bool) -> Tuple[np.ndarray, np.ndarray]:
+        r = 1.0
+        fss = []
+        gradss = []
+        for q in qs:
+            fs = np.expand_dims(np.sum(q**2) - r**2, axis=0)
+            grads = np.expand_dims(2 * q, axis=0)
+            fss.append(fs)
+            gradss.append(grads)
+        return np.array(fss), np.array(gradss)
+
+
+def test_TrajectoryInequalityConstraint():
+    const = CircleConstraint()
+
+    ineq_const = TrajectoryInequalityConstraint.create_homogeneous(3, 2, const)
+    points = np.array([[-1.0, 0.9], [0.0, 0.9], [1.0, 0.9]])
+    f, _ = ineq_const.evaluate(points.flatten())
+    np.testing.assert_almost_equal((f > 0), np.array([True, False, True]))
+
+    ineq_const = TrajectoryInequalityConstraint.create_homogeneous(4, 2, const)
+    points = np.array([[-1.0, 0.9], [-0.5, 0.9], [0.5, 0.9], [1.0, 0.9]])
+    f, _ = ineq_const.evaluate(points.flatten())
+    np.testing.assert_almost_equal((f > 0), np.array([True, True, True, True]))
+
+    # test with motion_step_box enabled
+    y = 1.0 - 1e-4
+    ineq_const.motion_step_box = np.ones(2) * 0.1
+    points = np.array([[-1.0, y], [-0.5, y], [0.55, y], [1.0, y]])
+    f, _ = ineq_const.evaluate(points.flatten())
+    n_eval_point = 5 + 11 + 6
+    assert len(f) == n_eval_point
+
+    # because [0.0, y] will be evaluated we have
+    assert sum(f < 0) == 1
