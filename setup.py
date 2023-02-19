59e4216b2a04f0528dac824049a3cdf2c86a585c
HiroIshida <spitfire.docomo@gmail.com>
2023-02-20 01:05:15 +0900

    fix: specify pyglet version

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ install_requires = [
     "osqp",
     "ompl-thin>=0.0.15",
     "selcol>=0.0.3.2",
+    "pyglet<2.0",  # waiting for https://github.com/iory/scikit-robot/pull/284 be marged
 ]
 
 setup(
