20a0077337580b1ef103aef0ea50d287e8f283ec
HiroIshida <spitfire.docomo@gmail.com>
2023-04-12 05:20:37 +0900

    feat: solver difficult humanoid problem using aux solver

--- example/humanoid.py
+++ example/humanoid.py
@@ -2,6 +2,7 @@ import time
 
 import numpy as np
 from skrobot.coordinates import Coordinates, rpy_matrix
+from skrobot.model.primitives import Box
 from skrobot.viewers import TrimeshSceneViewer
 from tinyfk import BaseType
 
@@ -9,6 +10,7 @@ from skmp.constraint import ConfigPointConst, PoseConstraint
 from skmp.robot.jaxon import Jaxon, JaxonConfig
 from skmp.robot.utils import set_robot_state
 from skmp.satisfy import satisfy_by_optimization_with_budget
+from skmp.solver.auxpoint_solver import AuxSolver, AuxSolverConfig
 from skmp.solver.interface import Problem
 from skmp.solver.nlp_solver import SQPBasedSolver, SQPBasedSolverConfig
 
@@ -20,17 +22,17 @@ bounds = config.get_box_const()
 jaxon = Jaxon()
 angle = -2.0
 start_coords_list = [
-    Coordinates([0, -0.15, 0]),
-    Coordinates([0, +0.15, 0]),
-    Coordinates([0, -0.15, 1.5], rot=rpy_matrix(0, angle, 0)),
-    Coordinates([0, +0.15, 1.5], rot=rpy_matrix(0, angle, 0)),
+    Coordinates([0, -0.2, 0]),
+    Coordinates([0, +0.2, 0]),
+    Coordinates([0, -0.3, 1.5], rot=rpy_matrix(0, angle, 0)),
+    Coordinates([0, +0.3, 1.5], rot=rpy_matrix(0, angle, 0)),
 ]
 
 goal_coords_list = [
-    Coordinates([0, -0.15, 0.3]),
-    Coordinates([0, +0.15, 0]),
-    Coordinates([0, -0.15, 1.5], rot=rpy_matrix(0, angle, 0)),
-    Coordinates([0, +0.15, 1.5], rot=rpy_matrix(0, angle, 0)),
+    Coordinates([0, -0.2, +0.3]),
+    Coordinates([0, +0.2, 0]),
+    Coordinates([0, -0.3, 1.5], rot=rpy_matrix(0, angle, 0)),
+    Coordinates([0, +0.3, 1.5], rot=rpy_matrix(0, angle, 0)),
 ]
 
 selcol_const = config.get_neural_selcol_const(jaxon)
@@ -48,41 +50,50 @@ res_goal = satisfy_by_optimization_with_budget(
 assert res_goal.success
 
 const_coords_list = [
-    Coordinates([0, +0.15, 0]),
-    Coordinates([0, -0.15, 1.5], rot=rpy_matrix(0, angle, 0)),
-    Coordinates([0, +0.15, 1.5], rot=rpy_matrix(0, angle, 0)),
+    Coordinates([0, +0.2, 0]),
+    Coordinates([0, -0.3, 1.5], rot=rpy_matrix(0, angle, 0)),
+    Coordinates([0, +0.3, 1.5], rot=rpy_matrix(0, angle, 0)),
 ]
 efkin_swing = config.get_endeffector_kin(rleg=False)
 eq_const_global = PoseConstraint.from_skrobot_coords(const_coords_list, efkin_swing, jaxon)
 problem = Problem(
-    res_start.q, bounds, ConfigPointConst(res_goal.q), None, eq_const_global, motion_step_box_=0.1
+    res_start.q,
+    bounds,
+    ConfigPointConst(res_goal.q),
+    selcol_const,
+    eq_const_global,
+    motion_step_box_=0.1,
 )
 
 print("start solving")
-# solver = OMPLSolver.init(
-#     OMPLSolverConfig(
-#         n_max_call=10000, simplify=True, algorithm=Algorithm.KPIECE1, algorithm_range=0.001
-#     )
-# )
-# solver.setup(problem)
-# res = solver.solve()
-# assert res.traj is not None
+
+solver = AuxSolver.init(AuxSolverConfig())
+solver.setup(problem)
+res = solver.solve()
+assert res.traj is not None
 
 sqp_solver = SQPBasedSolver.init(
     SQPBasedSolverConfig(
-        30, n_max_call=100, motion_step_satisfaction="explicit", verbose=True, ctol_eq=1e-3
+        20, n_max_call=100, motion_step_satisfaction="explicit", verbose=True, ctol_eq=1e-3
     )
 )
 sqp_solver.setup(problem)
-sqp_res = sqp_solver.solve()
-assert sqp_res.traj is not None
+res = sqp_solver.solve(res.traj)
+assert res.traj is not None
+print(res.time_elapsed)
 
-traj = sqp_res.traj.resample(30)
+traj = res.traj.resample(10)
+
 
 vis = TrimeshSceneViewer()
 vis.add(jaxon)
+for i in range(15):
+    box = Box([0.1, 1.0, 0.05])
+    box.translate([0.1, 0.0, -0.9 + 0.3 * i])
+    vis.add(box)
 vis.show()
 time.sleep(15)
+
 
 for q in traj:
     print(q)
