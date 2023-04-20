48d042baa54270d571e74ec8151883292ac7636c
HiroIshida <spitfire.docomo@gmail.com>
2023-04-21 04:16:30 +0900

    example: update jaxon demo

--- example/humanoid2.py
+++ example/humanoid2.py
@@ -18,10 +18,12 @@ from skmp.robot.utils import set_robot_state
 from skmp.satisfy import satisfy_by_optimization_with_budget
 from skmp.solver.interface import Problem
 from skmp.solver.myrrt_solver import MyRRTConfig, MyRRTConnectSolver
+from skmp.solver.nlp_solver import SQPBasedSolver, SQPBasedSolverConfig
 
-# np.random.seed(5)
+np.random.seed(5)
 
 com_box = Box([0.25, 0.5, 5.0], with_sdf=True)
+com_box.visual_mesh.visual.face_colors = [255, 0, 100, 100]
 
 box = Box([1.0, 3.0, 0.1], with_sdf=True)
 box.translate([0.8, 0.0, 1.1])
@@ -40,7 +42,7 @@ selcol_const = config.get_neural_selcol_const(jaxon)
 colkin = config.get_collision_kin()
 col_const = CollFreeConst(colkin, box.sdf, jaxon)
 com_const = config.get_com_stability_const(jaxon, lambda x: -com_box.sdf(x))
-ineq_const = IneqCompositeConst([col_const, selcol_const, com_const])
+ineq_const = IneqCompositeConst([com_const, col_const, selcol_const])
 
 efkin = config.get_endeffector_kin()
 eq_const_start = PoseConstraint.from_skrobot_coords(start_coords_list, efkin, jaxon)
@@ -84,12 +86,30 @@ problem = Problem(
     eq_const_path_plan,
     motion_step_box_=0.1,
 )
+print("start solving rrt")
 rrt = MyRRTConnectSolver.init(MyRRTConfig(10000))
 rrt.setup(problem)
 result = rrt.solve()
 assert result.traj is not None
 print(result)
 print(time.time() - ts)
+
+print("smooth out the result")
+solver = SQPBasedSolver.init(
+    SQPBasedSolverConfig(
+        n_wp=60,
+        n_max_call=200,
+        motion_step_satisfaction="explicit",
+        verbose=True,
+        ctol_eq=1e-3,
+        ctol_ineq=1e-3,
+        ineq_tighten_coef=0.0,
+    )
+)
+solver.setup(problem)
+result = solver.solve(result.traj)
+assert result.traj is not None
+print(result.time_elapsed)
 
 vis = TrimeshSceneViewer()
 vis.add(box)
@@ -101,5 +121,4 @@ for q in result.traj.resample(20):
     set_robot_state(jaxon, config._get_control_joint_names(), q, base_type=BaseType.FLOATING)
     time.sleep(0.5)
     vis.redraw()
-
 time.sleep(10)
