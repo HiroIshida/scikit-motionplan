bf6f86f4e8fad55a80ecb1311ea27243904855a3
HiroIshida <spitfire.docomo@gmail.com>
2023-04-30 16:14:29 +0900

    perf: cache urdf model without geometry and load it by default

--- skmp/utils.py
+++ skmp/utils.py
@@ -5,16 +5,28 @@ from typing import Dict
 from skrobot.utils.checksum import checksum_md5
 from skrobot.utils.urdf import URDF
 
+_loaded_urdf_models_with_geometry: Dict[str, URDF] = {}
 _loaded_urdf_models: Dict[str, URDF] = {}
 
 
-def load_urdf_model_using_cache(file_path: Path):
+def load_urdf_model_using_cache(file_path: Path, with_geometry: bool = False):
     file_path = file_path.expanduser()
     assert file_path.exists()
 
     hashvalue = checksum_md5(str(file_path))
 
-    if hashvalue not in _loaded_urdf_models:
+    if hashvalue not in _loaded_urdf_models_with_geometry:
         urdf = URDF.load(str(file_path))
-        _loaded_urdf_models[hashvalue] = urdf
-    return copy.deepcopy(_loaded_urdf_models[hashvalue])
+        _loaded_urdf_models_with_geometry[hashvalue] = urdf
+
+        # create urdf without mesh, which is easy to copy
+        urdf_without_geometry: URDF = copy.deepcopy(urdf)
+        for link in urdf_without_geometry._links:
+            link.visuals = None
+            link.collisions = None
+        _loaded_urdf_models[hashvalue] = urdf_without_geometry
+
+    if with_geometry:
+        return copy.deepcopy(_loaded_urdf_models_with_geometry[hashvalue])
+    else:
+        return copy.deepcopy(_loaded_urdf_models[hashvalue])
