699f9e094b73a58ccecf3c00b3f1544392342a79
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-03-13 22:08:32 +0900

    feat(datadrive): add chunked dataset for nn solver

--- skmp/solver/datadriven.py
+++ skmp/solver/datadriven.py
@@ -1,7 +1,14 @@
+import json
+import pickle
+import shutil
+import subprocess
+import tempfile
 from dataclasses import dataclass
-from typing import List, Optional, Tuple, Type, TypeVar
+from pathlib import Path
+from typing import Generic, List, Optional, Tuple, Type, TypeVar, Union, Sequence
 
 import numpy as np
+import tqdm
 
 from skmp.solver.interface import (
     AbstractScratchSolver,
@@ -15,6 +22,122 @@ from skmp.trajectory import Trajectory
 ConfigT = TypeVar("ConfigT", bound="ConfigProtocol")
 ResultT = TypeVar("ResultT", bound="ResultProtocol")
 
+DataT = TypeVar("DataT")
+
+
+@dataclass
+class ChunkedLibrary(Generic[DataT]):
+    chunk_list: List[Optional[List[DataT]]]  # at least last chunk is not None
+    edit_flags: List[bool]
+    file_path: Path
+    n_per_chunk: int
+    _td_path: Path
+
+    def __post_init__(self):
+        assert self.file_path.suffix == ".tar"
+        assert self.n_per_chunk > 0
+        assert self._td_path.is_dir()
+
+    def __del__(self):
+        shutil.rmtree(self._td_path)
+
+    @classmethod
+    def create(cls, file_path: Union[Path, str], n_per_chunk: int = 32768) -> "ChunkedLibrary":
+        if isinstance(file_path, str):
+            file_path = Path(file_path)
+        td = tempfile.mkdtemp()
+        td_path = Path(td)
+        return cls([[]], [True], file_path, n_per_chunk, td_path)
+
+    @classmethod
+    def load(cls, file_path: Union[Path, str]) -> "ChunkedLibrary":
+        if isinstance(file_path, str):
+            file_path = Path(file_path)
+        assert file_path.suffix == ".tar"
+        td = tempfile.mkdtemp()
+        td_path = Path(td)
+        subprocess.run(["tar", "-xf", str(file_path), "-C", str(td_path)], check=True)
+
+        metadata_path = td_path / "metadata.json"
+        with open(metadata_path, "r") as f:
+            metadata = json.load(f)
+        n_per_chunk = metadata["n_per_chunk"]
+        n_chunk = metadata["n_chunks"]
+        chunk_list: List[Optional[List[DataT]]]
+        if n_chunk == 0:
+            chunk_list = []
+        else:
+            chunk_list = [None] * (n_chunk - 1)
+            last_chunk_path = td_path / f"chunk_{n_chunk - 1}.pkl"
+            with open(last_chunk_path, "rb") as f:
+                chunk = pickle.load(f)
+                assert (
+                    len(chunk) == metadata["n_last_chunk"]
+                ), f"{len(chunk)} != {metadata['n_last_chunk']}"
+                chunk_list.append(chunk)
+        edit_flags = [False] * n_chunk
+        return cls(chunk_list, edit_flags, file_path, n_per_chunk, td_path)
+
+    def __getitem__(self, idx: int) -> DataT:
+        chunk_idx, idx_in_chunk = divmod(idx, self.n_per_chunk)
+        if self.chunk_list[chunk_idx] is None:
+            file_path = self._td_path / f"chunk_{chunk_idx}.pkl"
+            with open(file_path, "rb") as f:
+                self.chunk_list[chunk_idx] = pickle.load(f)
+        chunk_this = self.chunk_list[chunk_idx]
+        assert chunk_this is not None
+        return chunk_this[idx_in_chunk]
+
+    def __len__(self) -> int:
+        if len(self.chunk_list) == 0:
+            return 0
+        last_chunk = self.chunk_list[-1]
+        assert last_chunk is not None
+        return (len(self.chunk_list) - 1) * self.n_per_chunk + len(last_chunk)
+
+    def append(self, data: DataT) -> None:
+        n = len(self)
+        idx_chunk, idx_in_chunk = divmod(n, self.n_per_chunk)
+
+        need_more_chunk = idx_chunk == len(self.chunk_list)
+        if need_more_chunk:
+            self.chunk_list.append([])
+            self.edit_flags.append(True)
+
+        self.edit_flags[idx_chunk] = True
+        self.chunk_list[idx_chunk].append(data)  # type: ignore[union-attr]
+
+    def save(self) -> None:
+        n = len(self)
+        idx_chunk, idx_in_chunk = divmod(n, self.n_per_chunk)
+        if idx_in_chunk == 0:
+            n_chunk = idx_chunk
+            n_last_chunk = self.n_per_chunk
+        else:
+            n_chunk = idx_chunk + 1
+            n_last_chunk = idx_in_chunk
+
+        metadata_path = self._td_path / "metadata.json"
+        with open(metadata_path, "w") as f:
+            idx_chunk, idx_in_chunk = divmod(len(self), self.n_per_chunk)
+            json_data = {
+                "n_chunks": n_chunk,
+                "n_last_chunk": n_last_chunk,
+                "n_per_chunk": self.n_per_chunk,
+            }
+            json.dump(json_data, f)
+
+        for i in tqdm.tqdm(range(n_chunk), desc="saving"):
+            if not self.edit_flags[i]:
+                continue
+            file_path = self._td_path / f"chunk_{i}.pkl"
+            with open(file_path, "wb") as f:
+                pickle.dump(self.chunk_list[i], f)
+
+        subprocess.run(
+            ["tar", "-cf", str(self.file_path), "-C", str(self._td_path), "."], check=True
+        )
+
 
 @dataclass
 class NearestNeigborSolver(AbstractSolver[ConfigT, ResultT, np.ndarray]):
@@ -26,6 +149,19 @@ class NearestNeigborSolver(AbstractSolver[ConfigT, ResultT, np.ndarray]):
     infeasibility_threshold: int
     previous_est_positive: Optional[bool] = None
     previous_false_positive: Optional[bool] = None
+
+    @classmethod
+    def from_chunked_library(
+        cls: Type["NearestNeigborSolver[ConfigT, ResultT]"],
+        cfdataset: ChunkedLibrary[Tuple[float, np.ndarray, Optional[Trajectory]]],
+        n_data_use: int,
+        solver_type: Type[AbstractScratchSolver[ConfigT, ResultT]],
+        config: ConfigT,
+        knn: int = 1,
+        infeasibility_threshold: Optional[int] = None,
+    ) -> "NearestNeigborSolver[ConfigT, ResultT]":
+        dataset = [cfdataset[i][1:] for i in range(n_data_use)]
+        return cls.init(solver_type, config, dataset, knn, infeasibility_threshold)
 
     @classmethod
     def init(
