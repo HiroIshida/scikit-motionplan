26b5a5040dd78d9f238fa741908c56a1928382ad
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-07-10 15:11:00 +0900

    add demo

--- skmp/robot/jaxon.py
+++ skmp/robot/jaxon.py
@@ -4,7 +4,8 @@ from pathlib import Path
 from typing import Dict, List, Optional
 
 import numpy as np
-from robot_descriptions.jaxon_description import URDF_PATH as JAXON_URDF_PATH
+
+# from robot_descriptions.jaxon_description import URDF_PATH as JAXON_URDF_PATH
 from skrobot.coordinates import CascadedCoords
 from skrobot.coordinates.math import rotation_matrix, rpy_angle
 from skrobot.model.primitives import Box
@@ -17,6 +18,8 @@ from skmp.kinematics import (
     ArticulatedCollisionKinematicsMap,
     ArticulatedEndEffectorKinematicsMap,
 )
+
+JAXON_URDF_PATH = str(Path("~/misc/jaxon_urdf_private/JAXON_RED.urdf").expanduser())
 
 
 class Jaxon(RobotModelFromURDF):
